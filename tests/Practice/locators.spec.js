260e134a89f390b4dac8b4c116248f75885caf01
Girishgktester <girishgk.tester@gmail.com>
2025-03-28 23:52:43 +0530

    locators can take me to EU

--- tests/Practice/locators.spec.js
+++ tests/Practice/locators.spec.js
@@ -19,6 +19,7 @@ test('Locating multiple webelements ', async ({ page }) => {
     await page.goto("https://demoblaze.com/index.html")
     const links = await page.$$('//a')    // return aall the links
     console.log(links.length)
+    // forloop  traditional approach
     for (const link of links) {
         const linktexts = await link.textContent();
         console.log("All the links " + linktexts)
@@ -26,19 +27,16 @@ test('Locating multiple webelements ', async ({ page }) => {
 
     await page.waitForTimeout(2000)
 
+    // for loop enhanced 
     const productname = await page.$$("//div[@id='tbodyid']//div/h4/a")
-
     for (const product of productname) {
-
         const pname = await product.textContent()
-
         console.log("Product names " + pname)
     }
 });
 
 
 test("Playwright built in Locators", async ({ page }) => {
-
     await page.goto("https://demoblaze.com/index.html");
     await page.locator("id=login2").click();
     await page.locator("#loginusername").fill("pavanol");
@@ -50,5 +48,35 @@ test("Playwright built in Locators", async ({ page }) => {
     await expect(element).toBeVisible();
     await page.waitForTimeout(3000)
 });
+
+test("Playwright built in Locators continued", async ({ page }) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    //getByTagName
+    let logo = await page.getByAltText("company-branding")
+    await expect(logo).toBeVisible();
+
+   
+    // getByPlaceHolder
+    const username = await page.getByPlaceholder("Username")
+    await expect(username).toBeVisible();
+    username.fill("Admin")
+
+    const password = await page.getByPlaceholder("Password")
+    await expect(password).toBeVisible();
+    password.fill("admin123")
+
+    //getByRole 
+    await page.getByRole("button", { type: 'submit' }).click();
+    const userlogged = await page.getByText("manda user")
+    await expect(userlogged).toBeVisible();
+    await page.waitForTimeout(2000)
+
+     //getBylable
+    //  const getlabel = await page.getByLabel("Username").textContent()
+    //  console.log("getlabel ---->"+getlabel)
+});
+
 
 
