4b4eef28e6271897dd9cc2c03973eccf6e1d516e
Girishgktester <girishgk.tester@gmail.com>
2025-04-02 15:45:29 +0530

    Tags

---
+++ tests/Practice/Tags.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test');
+
+
+test('Test 1@smoke', async ({ page }) => {
+
+    console.log("Test1")
+});
+
+test('Test 2@smoke', async ({ page }) => {
+
+    console.log("Test2")
+});
+
+test('Test 3@sanity', async ({ page }) => {
+
+    console.log("Test3")
+});
+
+test('Test 4@sanity', async ({ page }) => {
+
+    console.log("Test4")
+});
