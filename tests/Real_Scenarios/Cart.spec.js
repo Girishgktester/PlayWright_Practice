ab99e8a4b9cb1767f43a69bee76555e924c15fa6
Girishgktester <girishgk.tester@gmail.com>
2025-04-06 21:34:26 +0530

    Testcase #19

--- tests/Real_Scenarios/Cart.spec.js
+++ tests/Real_Scenarios/Cart.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Testcase 11:  Verify Subscription in Cart page', async ({ page }) => {
     await page.goto("https://automationexercise.com/");
 
-    await page.getByText(" Cart").first().click({strict:true});
+    await page.getByText(" Cart").first().click({ strict: true });
 
     await page.getByText("Subscription").scrollIntoViewIfNeeded();
 
@@ -11,52 +11,52 @@ test('Testcase 11:  Verify Subscription in Cart page', async ({ page }) => {
 
     await page.keyboard.press("Enter")
 
-    await  expect(page.getByText("You have been successfully subscribed!")).toBeVisible();
+    await expect(page.getByText("You have been successfully subscribed!")).toBeVisible();
 
-   
+
 });
 
 
 test('Testcase 17: View Category Products', async ({ page }) => {
-   
+
     await page.goto("https://automationexercise.com/");
     const screen = await page.evaluate(() => {
         return {
-          width: window.screen.width,
-          height: window.screen.height
+            width: window.screen.width,
+            height: window.screen.height
         };
-      });
+    });
 
     //   const accordianBox = await page.locator('#accordian');
     // const box = await accordianBox.boundingBox();
-      
-      const leftQuarterMax = screen.width / 4;
-      const box = await page.locator('#accordian').boundingBox();
-      
-      if (box) {
+
+    const leftQuarterMax = screen.width / 4;
+    const box = await page.locator('#accordian').boundingBox();
+
+    if (box) {
         console.log(`Element x position: ${box.x}`);
         expect(box.x).toBeLessThanOrEqual(leftQuarterMax);
-      } else {
+    } else {
         throw new Error("Element not found or not visible.");
-      }
-   
-      await page.locator("//a[normalize-space()='Women']").first().click();
-   
-      await page.getByText("Dress ").first().click();
-   
-      await expect(page.getByText("Women - Dress Products").first()).toBeVisible()
+    }
 
-      await page.locator("//a[normalize-space()='Men']").click();
+    await page.locator("//a[normalize-space()='Women']").first().click();
 
-      await page.getByText("Tshirts ").first().click();
+    await page.getByText("Dress ").first().click();
 
-      await expect(page.getByText("Men - Tshirts Products")).toBeVisible();
+    await expect(page.getByText("Women - Dress Products").first()).toBeVisible()
+
+    await page.locator("//a[normalize-space()='Men']").click();
+
+    await page.getByText("Tshirts ").first().click();
+
+    await expect(page.getByText("Men - Tshirts Products")).toBeVisible();
 
 });
 
 
 test('Testcase 11:  Remove Products From Cart', async ({ page }) => {
-   
+
     await page.goto("https://automationexercise.com/");
     await page.locator('[data-product-id="1"]').first().hover();
     await page.locator('[data-product-id="1"]').first().click();
@@ -64,5 +64,16 @@ test('Testcase 11:  Remove Products From Cart', async ({ page }) => {
     await page.getByText("View Cart").first().click();
     await page.locator('.cart_quantity_delete').click();
     await expect(page.locator('p.text-center')).toHaveText("Cart is empty! Click here to buy products.");
-   
+
 });
+
+test('Testcase 19: View & Cart Brand Products', async ({ page }) => {
+
+    await page.goto("https://automationexercise.com/");
+    await page.locator("a[href='/products']").click();
+    await page.getByText("Polo").first().click();
+    await expect(page.getByText("Brand - Polo Products")).toBeVisible();
+    await page.getByText("H&M").click();
+    await expect(page.getByText("Brand - H&M Products")).toBeVisible();
+});
+
