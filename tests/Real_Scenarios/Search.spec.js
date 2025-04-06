5f4d17f39b539a4be9ae3fda3e0291209baf5466
Girishgktester <girishgk.tester@gmail.com>
2025-04-06 18:32:29 +0530

    Testcase #9

---
+++ tests/Real_Scenarios/Search.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import exp from 'constants';
+
+test('Testcase 9: Search for product', async ({ page }) => {
+    await page.goto("https://automationexercise.com/");
+
+    await page.locator("a[href='/products']").click();
+
+    const searchbox =  page.getByRole("textbox", {name:'Search Product'})
+
+    expect(searchbox).toBeVisible();
+    searchbox.fill("Blue Top")
+
+    await page.locator("#submit_search").click();
+
+    await expect(page.getByText("Searched Products")).toBeVisible();
+
+    await expect(page.locator("(//p[text()='Blue Top'])[2]")).toBeVisible();
+
+
+});
