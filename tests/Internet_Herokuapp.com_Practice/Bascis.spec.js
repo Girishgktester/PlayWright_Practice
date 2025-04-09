6ed3b578308f4818fd140100a7fdec4bb37f0102
Girishgktester <girishgk.tester@gmail.com>
2025-04-09 14:06:52 +0530

    Testcase 2 Checkbox

--- tests/Internet_Herokuapp.com_Practice/Bascis.spec.js
+++ tests/Internet_Herokuapp.com_Practice/Bascis.spec.js
@@ -7,12 +7,23 @@ test('Testcase 1: Add/Remove Elements', async ({ page }) => {
     const addElement = await page.locator("button[onclick='addElement()']");
 
     for (let i = 0; i < 10; i++) {
-
         await page.waitForTimeout(1000)
         await addElement.click();
         const deleteButtons = await page.$$('button', { hasText: 'Delete' })  // all buttons with "Delete" text
         console.log("Number of Delete Buttons added per iteration" + deleteButtons.length)
-
     }
+});
 
+
+test('Testcase 2: Checkboxes', async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/checkboxes");
+
+    const checkbox1 = page.locator('//form[@id="checkboxes"]/input[1]');
+    await checkbox1.check();
+    await expect(checkbox1).toBeChecked();
+    const checkbox2 = page.locator('//form[@id="checkboxes"]/input[2]');
+    await checkbox2.uncheck();
+    await page.waitForTimeout(2000)
+    await expect(checkbox2).not.toBeChecked();
+    await page.waitForTimeout(2000)
 });
